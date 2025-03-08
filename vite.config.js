953094126f76f1fd5e9fec72b62f3aa2b73e55f7
Agnieszka335 <agua3355@gmail.com>
2025-03-08 17:42:44 +0100

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+/**
+ * Path to exercise folder
+ */
+const exercisePath = "05_Dzien_3/04_Flexbox/01_Verti";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+  root: exercisePath,
+  server: {
+    port: 3000,
+  },
+});
