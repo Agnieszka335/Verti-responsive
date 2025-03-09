a8246e9c690f563a8f7d36794e71e3f253ee300a
Agnieszka335 <agua3355@gmail.com>
2025-03-09 16:21:34 +0100

    new commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */
-const exercisePath = "Coderslab/Verti-fully-responsive/01_Verti/index.html";
+const exercisePath = "Verti";
 
 /**
  * Don't change those lines below
